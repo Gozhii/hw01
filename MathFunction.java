f9038070ef5367a3228137f4631610144201cb57
PavelGozhii <36234557+PavelGozhii@users.noreply.github.com>
2019-03-26 17:26:07 +0200

    Update MathFunction.java

--- MathFunction.java
+++ MathFunction.java
@@ -2,18 +2,19 @@ package com.company.hw01;
 
 public class MathFunction {
 
-    public double getPow(float x, double pow){
+    public double getPow(float x, double pow) {
+        double inner_pow = pow;
         double result = x;
-        if(pow > 0){
-            for(int i = 1; i < pow; i ++){
-                result*=x;
-            }
+        if (pow < 0) {
+            inner_pow = -pow;
+        }
+        for (int i = 1; i < inner_pow; i++) {
+            result *= x;
+        }
+        if (pow > 0) {
             return result;
-        }else if(pow < 0){
-            for(int i = 1; i < -pow; i ++){
-                result*=x;
-            }
-            return 1/result;
+        } else if (pow < 0) {
+            return 1 / result;
         }
         return 1;
     }
