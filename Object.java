076bbfde3f53f3894fa2df7b74e146e737f837bf
PavelGozhii <36234557+PavelGozhii@users.noreply.github.com>
2019-03-26 17:27:37 +0200

    Update Object.java

--- Object.java
+++ Object.java
@@ -5,13 +5,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-//The class is called Object because I implement a method of this class
-public class Object {
 
-    //Оur lecturer said to implement only deep cloning(maybe I something not so understood)
-    //If I use this method, the new instance already has a new hash code as well as its fields.
-    // Otherwise, they are identical to the old instance
-    public static Object deepClone(Object object){
+//The interface of the method, which allows a deep clone
+public interface deepClonanble {
+    static Object deepClone(Object object) {
         try {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             ObjectOutputStream outputStrm = new ObjectOutputStream(outputStream);
@@ -19,8 +16,7 @@ public class Object {
             ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
             ObjectInputStream objInputStream = new ObjectInputStream(inputStream);
             return (Object) objInputStream.readObject();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
