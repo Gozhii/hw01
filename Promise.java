4616ff3bf6e569820b330f5694af79d524841214
PavelGozhii <36234557+PavelGozhii@users.noreply.github.com>
2019-03-26 17:28:24 +0200

    Update Promise.java

--- Promise.java
+++ Promise.java
@@ -1,7 +1,7 @@
 package com.company.hw01;
 
-public class Promise{
-    public int resultOfMatch(int firstResult, int secondResult, int firstPromise, int secondPromise){
+public class Promise {
+    public int resultOfMatch(int firstResult, int secondResult, int firstPromise, int secondPromise) {
         return (firstResult == firstPromise) && (secondResult == secondPromise) ? 2 :
                 (firstResult > secondResult && firstPromise > secondPromise)
                         || (firstResult <= secondResult && firstPromise <= secondPromise) ? 1 : 0;
